08fc1d9a3d1f46d99ada8f841a002a49f78dc5d2
bannikov_ilya <101068317+b4ntik@users.noreply.github.com>
2025-08-04 12:46:42 +0500

    add tests

---
+++ src/test/java/org/courseWork/CourseWorkTests.java
@@ -0,0 +1,13 @@
+package org.courseWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
